e466e0d4966848b71882ea239e185a94b4bd10cb
pbchen <294306275@qq.com>
2015-11-25 10:04:04 +0800

    change password

--- application/views/member/change_password.php
+++ application/views/member/change_password.php
@@ -5,7 +5,7 @@
 
 <div class="row-fluid">
     <div class="span6">
-        <form class="form-horizontal well" action="/member/change_password" method="post" id="change_password_form" onsubmit="return check();">
+        <form class="form-horizontal well" name="form" action="/member/change_password" method="post" onSubmit="return check();">
             <fieldset>
                 <div class="form-group">
                     <label class="col-lg-2 control-label">旧密码</label>
@@ -13,7 +13,7 @@
                         <input type="password" id="old-password" class="form-control"/>
                     </div>
                     <label class="col-lg-2 control-label" id="old-password-error" style="text-align: left;color: red;">
-                        <?php if($old_msg) echo $old_msg;?>
+                        <?php if ($old_msg) echo $old_msg; ?>
                     </label>
                 </div>
                 <div class="form-group">
@@ -22,7 +22,7 @@
                         <input type="password" id="password" class="form-control"/>
                     </div>
                     <label class="col-lg-2 control-label" id="password-error" style="text-align: left;color: red;">
-                        <?php if($msg) echo $msg;?>
+                        <?php if ($msg) echo $msg; ?>
                     </label>
                 </div>
                 <div class="form-group">
@@ -36,30 +36,25 @@
 </div>
 
 <script>
-
-    $(function () {
-        function  check() {
-            var old_pwd = $("#old-password").val();
-            var new_pwd = $("#password").val();
-            var new_pwd_len = new_pwd.length;
-            $("#old-password-error").text('');
-            $("#password-error").text('');
-            if (old_pwd === '') {
-                $("#old-password-error").text('请输入旧密码！');
-                return false;
-            }
-            if (new_pwd == '') {
-                $("#password-error").text('请输入新密码！');
-                return false;
-            }
-            if (new_pwd!='' && (new_pwd_len<6 || new_pwd_len>8)) {
-                $("#password-error").text('新密码长度应在6-8位！');
-                return false;
-            }
-            return true;
+    function  check() {
+        var old_pwd = $("#old-password").val();
+        var new_pwd = $("#password").val();
+        var new_pwd_len = new_pwd.length;
+        $("#old-password-error").text('');
+        $("#password-error").text('');
+        if (old_pwd === '') {
+            $("#old-password-error").text('请输入旧密码！');
+            return false;
         }
-
-    });
-
+        if (new_pwd == '') {
+            $("#password-error").text('请输入新密码！');
+            return false;
+        }
+        if (new_pwd != '' && (new_pwd_len < 6 || new_pwd_len > 8)) {
+            $("#password-error").text('新密码长度应在6-8位！');
+            return false;
+        }
+        return true;
+    }
 </script>
 
