0a32953b9f432a1cd2b980f6122df25fad2598a6
pbchen <294306275@qq.com>
2015-12-05 11:51:25 +0800

    礼品卡

---
+++ application/models/giftcard_model.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of giftcard_model
+ *
+ * @author pbchen
+ */
+class giftcard_model extends CI_Model {
+    function __construct() {
+        parent::__construct();
+    }
+}
